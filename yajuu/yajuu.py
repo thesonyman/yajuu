5fa2d34daf9f3f974e437cb39512be99cbb1d9f8
Vivescere <vivescere@gmail.com>
2016-05-28 15:10:31 +0200

    Base cli
    The cli uses docopt for now. It can show the version, the help, and just print the arguments for the defined commands.
    The defined  commands are for now 'media list' and 'media download'.

--- yajuu/yajuu.py
+++ yajuu/yajuu.py
@@ -1,14 +1,28 @@
-"""Provides entry point main()."""
+'''Yajuu is an automated media downloader.
 
-import sys
+Usage:
+    yajuu media list <media>
+    yajuu media download <media> <name>
+    yajuu (-h | --help)
+    yajuu --version
+'''
+
 import pkg_resources
+from docopt import docopt
 
 
 __version__ = pkg_resources.require('yajuu')[0].version
 
 
 def main():
-    print("List of argument strings: %s" % sys.argv[1:])
+    arguments = docopt(__doc__)
 
-    if len(sys.argv[1:]) > 0 and sys.argv[1] == 'version':
-        print('Version: {}'.format(__version__))
+    if arguments['--version']:
+        print('Yajuu {}'.format(__version__))
+    elif arguments['media']:
+        if arguments['list']:
+            print('Listing media {}'.format(arguments['<media>']))
+        elif arguments['download']:
+            print('Download media {}, the query is {}'.format(
+                arguments['<media>'], arguments['<name>']
+            ))
