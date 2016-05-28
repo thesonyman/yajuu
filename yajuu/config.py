d2e5b66eba0136448d308be9a94507c2b5a3bc1b
Vivescere <vivescere@gmail.com>
2016-05-28 14:23:23 +0200

    Config reading, including a base object.
    The added config.py file will try to load the config file from ~/.config/yajuu/config.yaml. If found, it will ensure that the configration is correct (not yet for data type, only existence). Else, it will create the file using the default configuration.
    Some default config arguments are included.

---
+++ yajuu/config.py
@@ -0,0 +1,56 @@
+"""Provides the config dict object. Will create it if not found."""
+
+import os
+import yaml
+
+
+DEFAULT_CONFIG = {
+    'thetvdb': {
+        'api_key': None,
+        'language': 'en'
+    },
+    'media': {
+        'preferred_quality': 1080,
+        'minimum_quality': 720,
+        'maximum_quality': None
+    },
+    'paths': {
+        'base': os.path.expanduser('~/Videos'),
+        'medias': {
+            'anime': {
+                'base': 'Anime',
+                'season': 'Season {season:02d}',
+                'episode': '{anime_name} - S{season:02d}e{number:03d}.mp4'
+            }
+        }
+    }
+}
+
+config_path = os.path.expanduser('~/.config/yajuu/config.yaml')
+
+
+def check_config(expected, given):
+    """Recursive function to check the configuration passed by the user."""
+
+    config = {}
+
+    for key, value in expected.items():
+        if key in given:
+            if type(value) == dict:
+                config[key] = check_config(value, given[key])
+            else:
+                config[key] = given[key]
+        else:
+            config[key] = value
+
+    return config
+
+
+if os.path.exists(config_path):
+    with open(config_path, 'r') as file:
+        config = check_config(DEFAULT_CONFIG, yaml.load(file))
+else:
+    config = DEFAULT_CONFIG
+
+    with open(config_path, 'w') as file:
+        file.write(yaml.dump(config, default_flow_style=False))
