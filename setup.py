24a34580582f4b8d0e5ccb91f369b1260e86de5f
Vivescere Discere <vivescere@gmail.com>
2016-06-24 19:14:29 +0200

    Updated setup.py to use requirements.txt and README.md.

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
 
 __version__ = '0.2.0'
 
-with open('README.rst', 'rb') as f:
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+
+with open('README.md', 'rb') as f:
     long_descr = f.read().decode('utf-8')
 
 setup(
@@ -15,5 +19,6 @@ setup(
     version=__version__,
     description='An automated media downloader.',
     long_description=long_descr,
-    author='Vivescere Discere'
+    author='Vivescere Discere',
+    install_requires=reqs
 )
