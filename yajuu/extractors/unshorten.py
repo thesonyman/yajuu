165e09dd2b914c6a484a2b3a69ca50b8ef5fcc01
Vivescere Discere <vivescere@gmail.com>
2016-07-05 19:22:40 +0200

    Added support for https://upload.af, used a lot on icefilms.info

--- yajuu/extractors/unshorten.py
+++ yajuu/extractors/unshorten.py
@@ -6,6 +6,7 @@ import urllib.parse
 import re
 import subprocess
 import base64
+import time
 
 import shlex
 import execjs
@@ -37,7 +38,8 @@ def unshorten(url, quality=None):
         'stream.moe': unshorten_stream_moe,
         'bakavideo.tv': unshorten_bakavideo,
         'drive.google.com': unshorten_google_drive,
-        'tusfiles.net': unshorten_tusfiles
+        'tusfiles.net': unshorten_tusfiles,
+        'upload.af': unshorten_upload_af
     }
 
     host = urllib.parse.urlsplit(url).netloc
@@ -315,6 +317,47 @@ def unshorten_tusfiles(url, quality=None):
 
     if quality is None:
         logger.warning('[tusfiles] quality was not passed')
+        quality = get_quality(src)
+
+    return [(quality, src)]
+
+def unshorten_upload_af(url, quality=None):
+    if not url.startswith('https'):
+        url = 'https' + url[4:]
+
+    def get_payload(source, selector, fields):
+        soup = BeautifulSoup(source, 'html.parser')
+        form = soup.select(selector)
+
+        payload = {}
+
+        for input in soup.find_all('input'):
+            if input.get('name') not in fields:
+                continue
+
+            payload[input.get('name')] = input.get('value')
+
+        return payload
+
+    download_1_payload = get_payload(
+        requests.get(url).text, 'form',
+        ['op', 'usr_login', 'id', 'fname', 'referer', 'method_free']
+    )
+
+    download_2_payload = get_payload(
+        requests.post(url, data=download_1_payload).text, 'form[name="F1"]',
+        ['op', 'usr_login', 'id', 'fname', 'referer', 'method_free']
+    )
+
+    soup = BeautifulSoup(
+        requests.post(url, data=download_2_payload).text,
+        'html.parser'
+    )
+
+    src = soup.select('.text-center a')[0].get('href')
+
+    if quality is None:
+        logger.warning('[upload.af] quality was not passed')
         quality = get_quality(src)
 
     return [(quality, src)]
