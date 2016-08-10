624ab24a884fda9b43fe1df8b54e894061e52193
Vivescere Discere <vivescere@gmail.com>
2016-08-10 14:44:43 +0200

    Added clarification to the media types

--- yajuu/media/types.py
+++ yajuu/media/types.py
@@ -1,8 +1,12 @@
+'''Provides binding from the media class to the orchestrator class.'''
+
 from yajuu.media.anime import Anime
 from yajuu.media.movie import Movie
 from yajuu.orchestrators.anime_orchestrator import AnimeOrchestrator
 from yajuu.orchestrators.movie_orchestrator import MovieOrchestrator
 
+
+# The keys here should be the same as in the config
 MEDIA_TYPES = {
     'anime': (Anime, AnimeOrchestrator),
     'movie': (Movie, MovieOrchestrator)
